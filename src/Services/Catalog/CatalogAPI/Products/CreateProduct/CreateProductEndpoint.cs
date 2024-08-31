8875b97b0d7116fd7fe29f7d179512f27f91acb7
DavidGPC <dcontre10@gmail.com>
2024-08-30 23:33:48 -0600

    5.50.1

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,10 @@
-﻿namespace CatalogAPI.Products.CreateProduct
+﻿namespace CatalogAPI.Products.CreateProduct;
+
+public record class CreateProductRequest(string Name, List<string> Categories, string Description, string ImageFile, decimal Price);
+
+public record class CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint
 {
-	public class CreateProductEndpoint
-	{
-	}
+
 }
