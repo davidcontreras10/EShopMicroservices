83ab812d827c80309d7ea05b258c07cb4f0c4c04
DavidGPC <dcontre10@gmail.com>
2024-09-01 14:57:19 -0600

    5.51.1

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,10 +1,15 @@
-﻿namespace CatalogAPI.Products.CreateProduct;
+﻿using Carter;
+
+namespace CatalogAPI.Products.CreateProduct;
 
 public record class CreateProductRequest(string Name, List<string> Categories, string Description, string ImageFile, decimal Price);
 
 public record class CreateProductResponse(Guid Id);
 
-public class CreateProductEndpoint
+public class CreateProductEndpoint : CarterModule
 {
-
+	public override void AddRoutes(IEndpointRouteBuilder app)
+	{
+		
+	}
 }
