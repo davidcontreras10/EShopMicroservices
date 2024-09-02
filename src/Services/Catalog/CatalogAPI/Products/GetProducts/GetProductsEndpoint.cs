75c8f401411303094966951aad3fb806faf098b3
DavidGPC <dcontre10@gmail.com>
2024-09-02 02:05:58 -0600

    6.66.1

--- src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,6 +1,22 @@
-﻿namespace CatalogAPI.Products.GetProducts
+﻿namespace CatalogAPI.Products.GetProducts;
+
+public record GetProductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint : CarterModule
 {
-	public class GetProductsEndpoint
+	public override void AddRoutes(IEndpointRouteBuilder app)
 	{
+		app.MapGet("/products", async (ISender sender) =>
+		{
+			var query = new GetProductsQuery();
+			var result = await sender.Send(query);
+			var response = result.Adapt<GetProductsResponse>();
+			return Results.Ok(response);
+		})
+		.WithName("GetProducts")
+		.Produces<GetProductsResponse>(StatusCodes.Status200OK, "application/json")
+		.ProducesProblem(StatusCodes.Status400BadRequest, "application/json")
+		.WithSummary("Get all products")
+		.WithDescription("Get all products in the catalog");
 	}
 }
