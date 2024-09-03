2d4bafeb9825bba6723b9b5a22293294770c5ee5
DavidGPC <dcontre10@gmail.com>
2024-09-02 22:19:51 -0600

    6.73.1

--- src/Services/Catalog/CatalogAPI/Products/GetProductsByCategory/GetProductByCategoryEndpoint.cs
+++ src/Services/Catalog/CatalogAPI/Products/GetProductsByCategory/GetProductByCategoryEndpoint.cs
@@ -1,6 +1,22 @@
-﻿namespace CatalogAPI.Products.GetProductsByCategory
+﻿namespace CatalogAPI.Products.GetProductsByCategory;
+
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryEndpoint : CarterModule
 {
-	public class GetProductByCategoryEndpoint
+	public override void AddRoutes(IEndpointRouteBuilder app)
 	{
+		app.MapGet("/products/category/{category}", async (ISender sender, string category) =>
+		{
+			var query = new GetProductByCategoryQuery(category);
+			var result = await sender.Send(query);
+			var response = result.Adapt<GetProductByCategoryResponse>();
+			return Results.Ok(response);
+		})
+		.WithName("GetProductByCategory")
+		.Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK, "application/json")
+		.ProducesProblem(StatusCodes.Status400BadRequest, "application/json")
+		.WithSummary("Get all products by category")
+		.WithDescription("Get all products in the catalog by category");
 	}
 }
